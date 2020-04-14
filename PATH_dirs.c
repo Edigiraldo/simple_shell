dd7acc7541ccc14abe26fe93fe3ffb4d3cad61da
sarias12 <1638@holbertonschool.com>
2020-04-14 23:23:11 +0000

    Function PATH_dirs

---
+++ PATH_dirs.c
@@ -0,0 +1,35 @@
+#include "Holberton.h"
+
+/**
+ * PATH_dirs - function that gets the paths where
+ * executable files might be.
+ *
+ * Return: array of paths.
+ */
+
+char **PATH_dirs(void)
+{
+	char *PATHS = NULL; /*PATH1:PATH2:...*/
+	char *IND_PATH = NULL; /*PATH1*/
+	char **PATH_arr = NULL;
+	int size_ptr = sizeof(char *), i = 2;
+
+	PATHS = getenv("PATH");
+
+	IND_PATH = strtok(PATHS, ":");
+
+	PATH_arr = malloc(size_ptr * 2);
+	/*if malloc == -1*/
+	PATH_arr[0] = IND_PATH;
+	IND_PATH = strtok(NULL, ":");
+	while (IND_PATH != NULL)
+	{
+		PATH_arr = realloc(PATH_arr, (i + 1) * size_ptr);
+		PATH_arr[i - 1] = IND_PATH;
+		IND_PATH = strtok(NULL, ":");
+		i++;
+	}
+		PATH_arr[i - 1] = NULL;
+
+	return (PATH_arr);
+}
